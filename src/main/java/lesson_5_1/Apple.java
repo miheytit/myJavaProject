4b0a10980c9cc6e3931faf2c40cc0cccfadf1eba
miheytit <105665230+miheytit@users.noreply.github.com>
2022-06-09 21:03:06 +0300

    Delete src/main/java/lesson_5_1 directory

--- src/main/java/lesson_5_1/Apple.java
+++
@@ -1,7 +0,0 @@
-package lesson_5_1;
-
-public class Apple extends Fruit {
-    public float getWeight() {
-        return 1.0f;
-    }
-}
--- src/main/java/lesson_5_1/Orange.java
+++
@@ -1,7 +0,0 @@
-package lesson_5_1;
-
-public class Orange extends Fruit {
-    public float getWeight() {
-        return 1.5f;
-    }
-}
